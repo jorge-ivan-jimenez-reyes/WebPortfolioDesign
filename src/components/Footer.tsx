fc5f75314f28a12c55e64651d53dfe52a89db961
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-02-23 13:52:17 -0600

    structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer
+            id="footer"
+            className="c-lesPJm c-lesPJm-iBYZKI-css"
+        >
+            {/* Your HTML content goes here.
+          Make sure to:
+          - Convert `class` to `className`
+          - Convert inline styles to object notation if needed
+          - Remove top-level <body> or <html> tags */}
+            <div aria-hidden="true" className="c-lesPJm c-lesPJm-ijodOwq-css"></div>
+            {/* ... more markup ... */}
+        </footer>
+    );
+};
+
+export default Footer;
