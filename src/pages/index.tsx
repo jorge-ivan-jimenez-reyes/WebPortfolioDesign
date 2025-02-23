789b3cb720bf37b334c393e50cad1b1e393b635f
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-02-23 17:49:46 -0600

    grid 3d view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+// src/pages/index.tsx
 import Custom3DGrid from '../components/Custom3DView';
+import Footer from '../components/Footer';
 
 export default function Home() {
     return (
-        <div className="w-screen h-screen">
+        <div className="w-screen h-screen relative">
             <Custom3DGrid />
+
+            {/* Footer Component */}
+            <Footer />
         </div>
     );
 }
