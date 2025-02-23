99ff490a6fa231c5eeda5ee859d660b7426b5312
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-02-23 13:58:51 -0600

    structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,17 @@
-// src/pages/_document.tsx
+// pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
-                <body style={{ overflow: 'hidden', overscrollBehavior: 'none' }}>
+                <Head>
+                    {/* Favicon */}
+                    <link rel="icon" href="/favicon.ico" />
+                    {/* Additional meta tags or links can go here */}
+                    <meta name="description" content="My Portfolio Website" />
+                </Head>
+                <body>
                 <Main />
                 <NextScript />
                 </body>
