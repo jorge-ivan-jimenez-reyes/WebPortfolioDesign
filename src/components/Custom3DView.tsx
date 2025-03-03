2a7afa6501adb87dadbee098018588d49e8bed48
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-03-02 22:03:17 -0600

    refactor: update letters in Custom3DView component

--- src/components/Custom3DView.tsx
+++ src/components/Custom3DView.tsx
@@ -8,8 +8,8 @@ interface PersonaInfo {
 }
 
 const personaInfo: PersonaInfo[] = [
-    { letter: 'G', info: 'Graphic Designer with a passion for creating visually stunning designs.' },
-    { letter: 'F', info: 'Frontend Developer skilled in React and modern web technologies.' },
+    { letter: 'J', info: 'Graphic Designer with a passion for creating visually stunning designs.' },
+    { letter: 'O', info: 'Frontend Developer skilled in React and modern web technologies.' },
     { letter: 'R', info: 'Responsive Design expert, ensuring optimal user experience across all devices.' },
     { letter: 'E', info: 'Enthusiastic learner, always staying up-to-date with the latest industry trends.' },
     { letter: 'S', info: 'Software Engineer with a strong foundation in computer science principles.' },
